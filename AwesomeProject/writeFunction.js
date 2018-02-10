6f990a2c0701c9d09c5eb4833739504884dde780
Johnny Tulane <ehenningervoss@me.com>
2018-02-10 14:43:06 -0500

    thanks for a great first hackathon

--- AwesomeProject/writeFunction.js
+++ AwesomeProject/writeFunction.js
@@ -14,75 +14,66 @@ CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFT
 IN THE SOFTWARE.
 */
 
-var line1 = 'Programa, ' + course.courseType + (','*30) + '\n';
-var line2 = 'Fecha Inicial, ' + course.initialDate + (','*30) + '\n';
-var line3 = 'Fecha Final, ' + course.finalDate + (','*30) + '\n';
-var line4 = 'Barrio/Ciudad, ' + course.area + (','*30) + '\n';
-var line5 = 'Coordinador Regional,' + (','*30) + '\n';
-var line6 = 'Nombre, Contacto' + (','*30) + '\n';
-var line7 = course.regCoord.name + ',' + course.regCoord.cont + (','*30) + '\n';
-var line8 = 'Entrenador/a,' + (','*30) + '\n';
-var line9 = 'Nombre, Contacto' + (','*30) + '\n';
-var line10 =  course.trainer.name + ',' + course.trainer.cont + (','*30) + '\n';
-var line11 = 'Organizacion Coordinante,' + course.partnerOrg + (','*30) + '\n';
-var line12 = 'Lugar,' + course.placeType + (','*30) + '\n';
-var line13 = 'Nombre de el Lugar,' + course.placeName + (','*30) + '\n';
-var line14 = 'Estructura,' + course.structure + (','*30) + '\n';
-var line15 = ','*31;
-var line16 = ',,,' + course.sessions + ','*(28-(course.sessions.length-1));
-var line17 = 'Participantes,,Fecha' + course.sessDates +
-','*(28-(course.sessions.length-1));
-var line18 = 'Nombre,Edad,Sexo'
-
-function CourseDPV(courseName) {
-  this.id = coursesRun;
-  coursesRun += 1;
-  storage.save ({
-    key: 'startUpState',
-    data: {
-      courRun: coursesRun
-    },
-    expires: NULL
-  });
-  this.courseType = 'DPV';
-  this.courseName = courseName;
-  this.initialDate = "";
-  this.finalDate = "";
-  this.area = "";
-  this.regCoord = { name:"", cont:"" };
-  this.trainer = { name:"", cont:"" };
-  this.partnerOrg = "";
-  this.placeType = "";
-  this.placeName = "";
-  this.structure = "";
-  this.students = [];
-  this.sessions = DPVSessions;
-}
-
 function writeFile(course) {
+
+  var line1 = 'Programa, ' + course.courseType + (','*30) + '\n';
+  var line2 = 'Fecha Inicial, ' + course.initialDate + (','*30) + '\n';
+  var line3 = 'Fecha Final, ' + course.finalDate + (','*30) + '\n';
+  var line4 = 'Barrio/Ciudad, ' + course.area + (','*30) + '\n';
+  var line5 = 'Coordinador Regional,' + (','*30) + '\n';
+  var line6 = 'Nombre, Contacto' + (','*30) + '\n';
+  var line7 = course.regCoord.name + ',' + course.regCoord.cont
+  + (','*30) + '\n';
+  var line8 = 'Entrenador/a,' + (','*30) + '\n';
+  var line9 = 'Nombre, Contacto' + (','*30) + '\n';
+  var line10 =  course.trainer.name + ',' + course.trainer.cont
+  + (','*30) + '\n';
+  var line11 = 'Organizacion Coordinante,' + course.partnerOrg
+  + (','*30) + '\n';
+  var line12 = 'Lugar,' + course.placeType + (','*30) + '\n';
+  var line13 = 'Nombre de el Lugar,' + course.placeName + (','*30) + '\n';
+  var line14 = 'Estructura,' + course.structure + (','*30) + '\n';
+  var line15 = ','*31;
+  var line16 = ',,,' + course.sessions + ','*(28-(course.sessions.length-1));
+  var line17 = 'Participantes,,Fecha' + course.sessDates +
+  ','*(28-(course.sessions.length-1));
+  var line18 = 'Nombre,Edad,Sexo'+ ','*29;
+
   var RNFS = require('react-native-fs');
   var path = RNFS.DocumentDirectoryPath + '/DreamProjectCourse.csv';
-  RNSF.writeFile(path, 'This should be the first line')
+  RNSF.writeFile(path, line1+line2+line3+line4+line5+line6+line7+line8+line9)
   .then ((success) => {
     console.log('file written');
   })
   .catch((err) => {
     console.log(err.message);
   });
-  RNSF.appendFile(path, 'This should be the first line')
+  RNSF.appendFile(path, line10+line11+line12+line13+line14+line15+line16+line17)
   .then ((success) => {
     console.log('file written');
   })
   .catch((err) => {
     console.log(err.message);
   });
-  RNSF.writeFile()
-  RNSF.readFile(RNFS.DocumentDirectoryPath+'/DreamProjectCourse.csv')
-  .then((result) => {
-    render() {
-        <View style={styles.container}>
-          <Text>{result}</Text>
-        </View>
-      );
+  RNSF.appendFile(path, line18)
+  .then ((success) => {
+    console.log('file written');
   })
+  .catch((err) => {
+    console.log(err.message);
+  });
+  var i;
+  for (i in course.students) {
+    ray = [course.students[i].name, course.students.age[i]];
+    ray += [course.students[i].gender];
+    ray += [course.students[i].attendence];
+    link = ray.join(',');
+    RNSF.appendFile(path, link)
+    .then ((success) => {
+      console.log('file written');
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+  }
 }
